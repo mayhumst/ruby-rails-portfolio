1972fb900691d24e91905e609a3383d2a2f87caf
Maya Humston <mayahumston@gmail.com>
2025-07-10 18:05:59 -0400

    add react and bootstrap

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
